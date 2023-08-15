f042832c33fbaeaff2dea0b752ee9bf7642cb262
Luis Cardona <luiscardona.159@gmail.com>
2023-08-14 21:10:30 -0500

    feat: add jest settings

---
+++ tests/demo.test.js
@@ -0,0 +1,19 @@
+// import React from 'react';
+// import {render} from "@testing-library/react-native";
+// import App from '../App';
+
+// let component;
+
+// describe("<App />", () => {
+//     beforeEach(() => {
+//         component = render(<App />);
+//     });
+
+//     Item("renderiza correctamente", () => {
+//         console.log(component);
+//     })
+// });
+
+test('Esta prueba no debe fallar', () =>{
+
+})
