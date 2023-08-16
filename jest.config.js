162c3d34cfb34832af027ac739c2ddf785cdf86a
Luis Cardona <luiscardona.159@gmail.com>
2023-08-15 20:59:27 -0500

    feat: add suit tests settings     jest native settings     testing library react native settings

---
+++ jest.config.js
@@ -0,0 +1,8 @@
+const {resolve} = require('path');
+
+module.exports = {
+  preset: 'react-native',
+  // this is specific to the Jest repo, not generally needed (the files we ignore will be in node_modules which is ignored by default)
+  transformIgnorePatterns: [resolve(__dirname, '../../packages'),
+  "jest-runner"],
+};
